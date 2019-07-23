d3cc7657609a67d357cf454cb09fa578c96d1185
pv03571 <pv03571@gmail.com>
2019-07-23 23:16:51 +0800

    checking PR

--- chapter2/boiling.go
+++ chapter2/boiling.go
@@ -7,4 +7,5 @@ const boilingF float64 = 212.0
 func main() {
 	var centigrade = (boilingF - 32) * 5 / 9
 	fmt.Printf("Temperature is %gF and temperature in centigrade is %gC", boilingF, centigrade)
+
 }
